e62f1c5350f79a1ec4f8ac6064077d2507bdf62c
ibilanchuk <partyzan1996@gmail.com>
2016-09-16 16:56:55 +0300

    1

--- webpack.config.js
+++ webpack.config.js
@@ -36,6 +36,12 @@ module.exports = {
       { test: /\.js$/, loader: 'babel', include: path.join(__dirname, 'lib') },
       { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "url-loader?limit=10000&minetype=application/font-woff" },
       { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file-loader" },
+      { test: /\.(jpe?g|png|gif|svg)$/i,
+        loaders: [
+        'file?hash=sha512&digest=hex&name=[hash].[ext]',
+        'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false'
+        ]
+      }
     ]
   },
   cssnext: {
